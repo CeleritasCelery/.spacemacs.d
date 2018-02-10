f843837683c6a43ad36987504bcb45bb7bb6c78b
CeleritasCelery <t.macman@gmail.com>
2018-02-10 07:14:02 -0800

    added helm unite hyrda
    need to expand this it include the spacemacs helm transient state

--- layers/config/local/general-config/general-config.el
+++ layers/config/local/general-config/general-config.el
@@ -76,7 +76,6 @@
 	(apply orig-fun args)))
 (advice-add 'evil-previous-line :around 'my/evil-previous-line)
 
-(require 's)
 (defun my/open-file-in-clipboard ()
   (interactive)
   (helm-find-files-1 (replace-regexp-in-string "\\s-+" "" (substring-no-properties (current-kill 0)))))
@@ -160,6 +159,67 @@
 ;;     ))
 
 ;; /p/hdk/rtl/models/shdk74/config_spm/config_spm-srvr10nm-snr_0p8-17ww49a
+
+(defhydra helm-like-unite (:hint nil
+                                 :color pink)
+  "
+Nav ^^^^^^^^^        Mark ^^          Other ^^       Quit
+^^^^^^^^^^------------^^----------------^^----------------------
+_K_ ^ ^ _k_ ^ ^     _m_ark           _v_iew         _i_: cancel
+^↕^ _h_ ^✜^ _l_     _t_oggle mark    _H_elp         _o_: quit
+_J_ ^ ^ _j_ ^ ^     _U_nmark all     _d_elete
+^^^^^^^^^^                           _f_ollow: %(helm-attr 'follow)
+"
+  ;; arrows
+  ("h" helm-beginning-of-buffer)
+  ("j" helm-next-line)
+  ("k" helm-previous-line)
+  ("l" helm-end-of-buffer)
+  ;; beginning/end
+  ("g" helm-beginning-of-buffer)
+  ("G" helm-end-of-buffer)
+  ;; scroll
+  ("K" helm-scroll-other-window-down)
+  ("J" helm-scroll-other-window)
+  ;; mark
+  ("m" helm-toggle-visible-mark)
+  ("t" helm-toggle-all-marks)
+  ("U" helm-unmark-all)
+  ;; exit
+  ("<escape>" keyboard-escape-quit "" :exit t)
+  ("o" keyboard-escape-quit :exit t)
+  ("i" nil)
+  ;; sources
+  ("}" helm-next-source)
+  ("{" helm-previous-source)
+  ;; rest
+  ("H" helm-help)
+  ("v" helm-execute-persistent-action)
+  ("d" helm-persistent-delete-marked)
+  ("f" helm-follow-mode))
+
+(define-key helm-map (kbd "C-o") 'helm-like-unite/body)
+
+(defun helm-persistent-delete-marked ()
+  "Kill buffer without quitting helm."
+  (interactive)
+  (if (equal (cdr (assoc 'name (helm-get-current-source)))
+             "Buffers")
+      (with-helm-alive-p
+        (helm-attrset 'kill-action
+                      '(helm-persistent-kill-buffers . never-split))
+        (helm-execute-persistent-action 'kill-action))
+    (user-error "Only works for buffers")))
+
+(defun helm-persistent-kill-buffers (_buffer)
+  (unwind-protect
+      (dolist (b (helm-marked-candidates))
+        (helm-buffers-persistent-kill-1 b))
+    (with-helm-buffer
+      (setq helm-marked-candidates nil
+            helm-visible-mark-overlays nil))
+    (helm-force-update (helm-buffers--quote-truncated-buffer
+                        (helm-get-selection)))))
 
 (defun set-model ()
   (interactive)
