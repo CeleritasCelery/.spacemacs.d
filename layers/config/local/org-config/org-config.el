944ebd628cb37913eb75b3c8d662951ffe330f63
CeleritasCelery <t.macman@gmail.com>
2018-03-30 13:56:10 -0700

    added capture templates and new todo states to org mode
    I am trying to switch over to a capture/refile workflow instead of a org-journal
    workflow

--- layers/config/local/org-config/org-config.el
+++ layers/config/local/org-config/org-config.el
@@ -1,16 +1,18 @@
-;;; org mode config -*- lexical-binding: t -*-
+;;; org mode config
 
 (setq org-src-tab-acts-natively t)
 (setq org-priority-faces '((?A . (:foreground "OrangeRed" :weight 'bold))
                            (?B . (:foreground "yellow3" :weight 'bold))
                            (?C . (:foreground "ForestGreen" :weight 'bold))))
 
-(setq org-todo-keywords '((sequence "TODO(t)" "DOING(g)" "|" "DONE(d)")
-                          (sequence "|" "CANCELED(c)")))
+(setq org-todo-keywords '((sequence "TODO(t)" "DOING(g)" "WAIT(w@)" "|" "DONE(d)")
+                          (sequence "|" "CANCELED(c@)")))
 
-(add-hook 'org-mode-hook (lambda ()
-                           (spacemacs/toggle-truncate-lines-off)
-                           (org-indent-mode 1)))
+(defun cel/org-truncate-line ()
+  (let ((inhibit-message t))
+    (toggle-truncate-lines)))
+(add-hook 'org-mode-hook 'cel/org-truncate-line)
+(add-hook 'org-mode-hook 'org-indent-mode)
 
 (defun cel/evil-org-todo-enter-insert-state (&rest args)
   (and (featurep 'evil-states)
@@ -41,16 +43,51 @@
   (sp-local-pair 'org-mode "=" "=") ;; org literal
   (sp-local-pair 'org-mode "*" "*"));; org bold
 
+(setq org-journal-file-format "%Y-%m-%d")
 (setq org-journal-carryover-items nil)
 (setq org-directory "~/org")
-(setq org-journal-dir (f-join org-directory "journal")) ;; keep all techical journals here
+(setq org-default-notes-file (expand-file-name "notes.org" org-directory))
+(setq org-journal-dir (expand-file-name "journal" org-directory)) ;; keep all techical journals here
 (setq org-agenda-file-regexp (rx bos
-                                 (or (1+ (in "0-9")) ;; numeric journal files
-                                     (and (not (in ".")) ;; regular org files (foo.org)
+                                 (or (1+ (in "-" digit)) ;; numeric journal files
+                                     (and (not (any ".")) ;; regular org files (foo.org)
                                           (0+ nonl) ".org"))
                                  eos))
 
-(setq org-agenda-files (cons org-directory (f-directories org-directory))) ;; where to search for TODO's
+(defun org-archive-done-tasks ()
+  (interactive)
+  (org-map-entries
+   (lambda ()
+     (org-archive-subtree)
+     (setq org-map-continue-from (outline-previous-heading)))
+   "/DONE" 'file)
+  (org-map-entries
+   (lambda ()
+     (org-archive-subtree)
+     (setq org-map-continue-from (outline-previous-heading)))
+   "/CANCELED" 'file))
+(spacemacs/set-leader-keys-for-major-mode 'org-mode "oa" 'org-archive-done-tasks)
+
+(add-to-list 'auto-mode-alist
+             (cons (rx "/journal/20"
+                       (= 2 (any digit)) ;; year
+                       "-" (= 2 (any digit)) ;; month
+                       "-" (= 2 (any digit)) eos) ;; day
+                   'org-journal-mode))
+
+(defun cel/filter-buffers (buffer-list)
+  "ignore certain types of buffers in helm mini"
+  (delq nil (mapcar
+             (lambda (buffer)
+               (cond
+                ((eq (with-current-buffer buffer major-mode)  'org-agenda-mode) nil)
+                ((eq (with-current-buffer buffer major-mode)  'org-journal-mode) nil)
+                (t buffer)))
+             buffer-list)))
+(advice-add 'helm-skip-boring-buffers :filter-return 'cel/filter-buffers)
+
+(setq org-agenda-files (--remove (s-contains? "org-html-themes" it)
+                                 (f-directories org-directory nil t))) ;; where to search for TODO's
 
 (spacemacs|define-transient-state org-journal
   :title "navigate org journals"
@@ -63,5 +100,20 @@
   ("q" nil :exit t))
 (spacemacs/set-leader-keys-for-major-mode 'org-journal-mode "jn" 'spacemacs/org-journal-transient-state/org-journal-open-next-entry)
 (spacemacs/set-leader-keys-for-major-mode 'org-journal-mode "jp" 'spacemacs/org-journal-transient-state/org-journal-open-previous-entry)
+
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline org-default-notes-file "Tasks")
+         "* TODO %?\n %t\n" :empty-lines 1)
+        ("l" "Todo Link" entry (file+headline org-default-notes-file "Tasks")
+         "* TODO %?\n %i\n %a" :empty-lines 1)
+        ("s" "Scheduled TODO" entry (file+headline org-default-notes-file "Tasks")
+         "* TODO %?\n  SCHEDULED: %^T\n" :empty-lines 1)
+        ("T" "Todo from Clipboard" entry (file+headline org-default-notes-file "Tasks")
+         "* TODO %?\n  %c" :empty-lines 1)
+        ("n" "Note" entry (file+headline org-default-notes-file "Notes")
+         "* %?" :empty-lines 1)
+        ("N" "Note with Clipboard" entry (file+headline org-default-notes-file "Notes")
+         "* %?\n   %c" :empty-lines 1)))
+(spacemacs/set-leader-keys "oo" 'org-capture)
 
 (provide 'org-config)
