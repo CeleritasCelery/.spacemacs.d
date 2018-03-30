5ee401087df0a609577bca738ff298cf297a3399
CeleritasCelery <t.macman@gmail.com>
2018-03-30 13:58:56 -0700

    integrated evil-cleverparens with evil-lisp-state

--- layers/config/packages.el
+++ layers/config/packages.el
@@ -36,11 +36,39 @@
     evil-lion
     helm
     (find-protocol-file :location local)
+    evil-lisp-state
     ))
 
 (defun config/pre-init-helm ()
   (spacemacs|use-package-add-hook helm
     :post-config
+(defun config/post-init-evil-lisp-state ()
+  (when (configuration-layer/package-usedp 'evil-cleverparens)
+    (with-eval-after-load 'evil-lisp-state
+      ;; remove this keybindings so they can be prefixs
+      (define-key evil-lisp-state-map (kbd "y") nil)
+      (define-key evil-lisp-state-map (kbd "c") nil)
+
+      ;; rebind some functions that we overwrote
+      (define-key evil-lisp-state-map (kbd "o") 'sp-absorb-sexp) ;; a
+      (define-key evil-lisp-state-map (kbd "ys") 'sp-copy-sexp) ;; y
+      (define-key evil-lisp-state-map (kbd "cu") 'sp-convolute-sexp) ;; c
+
+      ;; add evil-cleverparens
+      (define-key evil-lisp-state-map (kbd "a") 'evil-cp-append)
+      (define-key evil-lisp-state-map (kbd "i") 'evil-cp-insert)
+
+      (define-key evil-lisp-state-map (kbd "Y") 'evil-cp-yank-line)
+      (define-key evil-lisp-state-map (kbd "D") 'evil-cp-delete-line)
+      (define-key evil-lisp-state-map (kbd "C") 'evil-cp-change-line)
+
+      (define-key evil-lisp-state-map (kbd "dd") 'evil-cp-delete)
+      (define-key evil-lisp-state-map (kbd "yy") 'evil-cp-yank)
+      (define-key evil-lisp-state-map (kbd "cc") 'evil-cp-change)
+
+      (define-key evil-lisp-state-map (kbd "x") 'evil-cp-delete-char-or-splice)
+      (define-key evil-lisp-state-map (kbd "X") 'evil-cp-delete-char-or-splice-backwards))))
+
     (define-key helm-map (kbd "C-S-k") 'helm-beginning-of-buffer)
     (define-key helm-map (kbd "C-S-j") 'helm-end-of-buffer)
     ))
